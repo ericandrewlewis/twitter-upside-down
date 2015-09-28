d4788b0866a64d94df7daed1e379904e9a50e571
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2015-09-28 07:54:33 -0400

    Split up modules into files

---
+++ message-list-view.js
@@ -0,0 +1,59 @@
+/**
+ * A view class for a chronological list of messages which presents the latest
+ * first with reverse infinite scroll.
+ *
+ * @constructor
+ */
+var MessageListView = function(args) {
+	this.messagesPerPage = args.messagesPerPage;
+	this.el = args.el;
+	this.messages = args.messages;
+	window.setInterval( this.checkToAddMore.bind( this ), 75 );
+};
+
+/**
+ * Load more messages into the DOM.
+ *
+ * Automatically deletes messages from the store.
+ */
+MessageListView.prototype.loadMore = function() {
+	if ( ! this.messages.length ) {
+		return;
+	}
+	var lowerBound = this.messages.length - this.messagesPerPage;
+	if ( lowerBound < 0 ) {
+		lowerBound = 0;
+	}
+	for ( var i = this.messages.length - 1; i >= lowerBound; i-- ) {
+		var div = document.createElement('div');
+		var textNode = document.createTextNode( '#' + i + ': ' + this.messages[i] );
+		div.appendChild( textNode );
+		if ( this.el.children ) {
+			this.el.insertBefore( div, this.el.firstChild );
+		} else {
+			this.el.appendChild( div );
+		}
+		// Remove the message from the store.
+		this.messages = this.messages.splice( 0, i );
+		// Don't let the new element modify previous scrolled state.
+		window.scrollTo( 0, window.scrollY + div.offsetHeight );
+	}
+};
+
+/**
+ * Check scroll position and load more messages as needed.
+ */
+MessageListView.prototype.checkToAddMore = function() {
+	if ( typeof this.lastScroll === 'undefined' ) {
+		this.lastScroll = window.scrollY;
+		return;
+	}
+	if ( this.lastScroll === window.scrollY ) {
+		return;
+	}
+	// Need to decide when to try to load more previous messages.
+	if ( window.scrollY < 500 ) {
+		this.loadMore();
+	}
+	this.lastScroll = window.scrollY;
+};
