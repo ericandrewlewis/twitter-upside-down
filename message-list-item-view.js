39128315d1d704367d0d791b4f895192b189cf4f
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2015-10-01 06:37:25 -0400

    Awesome reverse infinite scroll

--- message-list-item-view.js
+++ message-list-item-view.js
@@ -13,12 +13,7 @@ MessageListItemView.prototype.render = function() {
 	var self = this;
 	this.el = document.createElement('div');
 	this.el.className = 'tweet';
-	var tweetHeader = document.createElement('div');
-	var tweetName = document.createElement('strong');
-	var tweetNameText = document.createTextNode( this.data.user.name );
-	tweetName.appendChild( tweetNameText );
-	this.el.appendChild( tweetName);
-
+	this.el.style.display = 'none';
 	var imgWrapper = document.createElement('div');
 	imgWrapper.className = 'avatar';
 	var img = document.createElement('img');
@@ -28,14 +23,21 @@ MessageListItemView.prototype.render = function() {
 	img.src = this.data.user.profile_image_url_https;
 	imgWrapper.appendChild( img );
 	this.el.appendChild( imgWrapper );
-	var textWrapper = document.createElement('div');
-	textWrapper.className = 'tweet-content';
+	var tweetContent = document.createElement('div');
+	tweetContent.className = 'tweet-content';
+
+	var tweetName = document.createElement('strong');
+	var tweetNameText = document.createTextNode( this.data.user.name );
+	tweetName.appendChild( tweetNameText );
+	tweetContent.appendChild( tweetName );
+
 	var textNode = document.createTextNode( this.data.text );
-	textWrapper.appendChild( textNode );
-	this.el.appendChild( textWrapper );
+	tweetContent.appendChild( textNode );
+	this.el.appendChild( tweetContent );
 };
 
 MessageListItemView.prototype.DOMContentLoaded = function() {
+	this.el.style.display = '';
 	// Don't let the new element modify previous scrolled state.
 	window.scrollTo( 0, window.scrollY + this.el.offsetHeight );
 }
